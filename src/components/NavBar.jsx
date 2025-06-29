d66484b571cbcc8d50e7dbdb3e6e598b53af1cc5
AnuragPandey01 <anuragpandey87187@gmail.com>
2025-06-29 09:10:33 +0530

    change homepage text, navbar styling and remove logo for now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function NavBar() {
   return (
-    <div className='flex items-center fixed bg-white/70  w-[80%] place-content-between shadow-2xl/10 rounded-full px-2 py-4 top-4 backdrop-blur-[1.5px] border border-white/30'>
-        <div className='flex items-center ml-5'>
-            <img src="/logo.svg" alt="logo" className='h-10 p-0.5 align-middle'/>
-            <div className='font-bold'>
-            DevGest
-            </div>
-        </div>
-        <button className='bg-white text-black rounded-full border-gray-300 shadow-md border px-7 py-2 w-max text-[16px] hover:bg-gray-200'>
-            Sign Up
-        </button>
+    <div className="flex items-center fixed bg-white/70  w-[80%] place-content-between rounded-full px-2 py-2 top-4 backdrop-blur-[1.5px] border border-gray-200">
+      <div className="flex items-center ml-5 text-blue-500 font-bold text-xl">
+        DevGest
+      </div>
+      <button className="bg-white text-black rounded-full border-gray-300 shadow-sm border px-5 py-2 w-max text-[16px] hover:bg-gray-200">
+        Sign Up
+      </button>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,35 +1,30 @@
-import NavBar from "../components/NavBar" 
+import NavBar from "../components/NavBar";
 
-  function HomePage() {
-    return (
-      <div className="bg-[#FAFAFA] min-h-screen flex flex-col items-center justify-center px-4 bg-[url('/bg.svg')] bg-auto "> 
-        <NavBar/>
-        <div className="flex-col gap-7 flex items-center text-center">
-          <h1 className="text-[52px] leading-[110%] font-bold tracking-[-0.52px] text-[#27272A]">
-            Create, collaborate, and <br />
-            scale your blogs and docs.
-          </h1>
-          <p className="mx-auto max-w-[652px] leading-[28px] text-[18px] text-[#52525B]">
-            Effortlessly build blogs, API docs, and product guides with Hashnode, with <br/>
-            the flexibility of a headless CMS and more.
-          </p>
-          <div className='flex gap-2'>
-            <button className='bg-[#212124] text-white rounded-full px-7 py-2 w-max text-[16px] hover:bg-black shadow-xl/20'>
-              Sign up for free
-            </button>
-            <button className='bg-white text-black rounded-full border-gray-200 border px-7 py-2 w-max text-[16px] hover:bg-gray-200'>
-              Talk to founders
-            </button>
-          </div>
-          <p className='text-gray-500 text-sm'>
-            No credit card required.
-          </p>
+function HomePage() {
+  return (
+    <div className="bg-[#FAFAFA] min-h-screen flex flex-col items-center justify-center px-4 bg-[url('/bg.svg')] bg-auto ">
+      <NavBar />
+      <div className="flex-col gap-7 flex items-center text-center">
+        <h1 className="text-[52px] leading-[110%] font-bold tracking-[-0.52px] text-[#27272A]">
+          Create, Collaborate, and <br />
+          Communicate
+        </h1>
+        <p className="mx-auto max-w-[652px] leading-[28px] text-[18px] text-[#52525B]">
+          Effortlessly build blogs, collaborate and form community using devgest
+        </p>
+        <div className="flex gap-2">
+          <button className="bg-[#212124] text-white rounded-full px-7 py-2 w-max text-[16px] hover:bg-black shadow-xl/20">
+            Sign up for free
+          </button>
+          <button className="bg-white text-black rounded-full border-gray-200 border px-7 py-2 w-max text-[16px] hover:bg-gray-200">
+            Talk to founders
+          </button>
         </div>
-        <div>
-          {/* <img src='/download.svg' alt='Pic1' className='h-auto'></img> */}
-        </div>
+        <p className="text-gray-500 text-sm">No credit card required.</p>
       </div>
-    )
-  }
+      <div></div>
+    </div>
+  );
+}
 
-  export default HomePage
+export default HomePage;
